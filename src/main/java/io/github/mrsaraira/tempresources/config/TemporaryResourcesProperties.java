857f0cd639b5b3a98938c6d9821dd98613994cb2
Takhsin Saraira <mrsarayra@gmail.com>
2023-03-19 14:19:30 +0200

    Update

--- src/main/java/io/github/mrsaraira/tempresources/config/TemporaryResourcesProperties.java
+++ src/main/java/io/github/mrsaraira/tempresources/config/TemporaryResourcesProperties.java
@@ -1,6 +1,5 @@
 package io.github.mrsaraira.tempresources.config;
 
-import io.github.mrsaraira.tempresources.files.TemporaryFilesCleanerService;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -14,14 +13,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class TemporaryResourcesProperties {
 
     /**
-     * Set temporary resources library configuration enabled
+     * Set Temporary resources library configuration enabled
      */
     private Boolean enabled;
 
     /**
-     * Set temporary files cleaner service enabled
-     *
-     * @see TemporaryFilesCleanerService
+     * Set Temporary files cleaner service enabled
      */
     private Boolean temporaryFilesCleanerServiceEnabled;
 
